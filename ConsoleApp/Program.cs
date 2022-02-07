15931baa5ad8082b642b0333e61977aed9b31185
Piotr Andrzejewski <acax.piotrandrzejewski@gmail.com>
2022-02-07 18:31:42 +0100

    Update output message

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Everyone!");
+        }
+    }
+}
+
